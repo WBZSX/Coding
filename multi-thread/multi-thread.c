362af55428237db637ade30878bf9f691cb2e643
WBZSX <15871767133@163.com>
2020-05-13 22:16:01 +0800

    练习多线程编程中的线程属性设置。

--- multi-thread/multi-thread.c
+++ multi-thread/multi-thread.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<semaphore.h>
 
 pthread_t threadID[5];
 int share_variable = 0;
 pthread_mutex_t mutex;
+int thread_exit_flag[5] = {1};
 
 static void *thread_print_func(void *pMsg)
 {
@@ -20,7 +22,7 @@ static void *thread_print_func(void *pMsg)
         sleep(1);
     }
 
-    pthread_exit(NULL);
+    thread_exit_flag[id] = 1;
     return;
 }
 
@@ -28,20 +30,50 @@ int main()
 {
     int ErrCode = 0;
 
+    pthread_attr_t thread_attr;
+
+    ErrCode = pthread_attr_init(&thread_attr);
+    if(0 != ErrCode)
+    {
+        printf("thread attr init fail.\n");
+        return 0;
+    }
+
+    int scope = PTHREAD_SCOPE_SYSTEM;
+    ErrCode = pthread_attr_setscope(&thread_attr, scope);
+    if(0 != ErrCode)
+    {
+        printf("thread attr set scope fail.\n");
+        return 0;
+    }
+
+    int detach = PTHREAD_CREATE_DETACHED;
+    ErrCode = pthread_attr_setdetachstate(&thread_attr, detach);
+    if(0 != ErrCode)
+    {
+        printf("thread attr set detachstate fail.\n");
+        return 0;
+    } 
+
     pthread_mutex_init(&mutex, NULL);
+
     printf("start creating a new thread.\n");
 
     for(int i =0; i < 5; i++)
     {
-        ErrCode = pthread_create(&threadID[i], NULL, thread_print_func, &i);
+        ErrCode = pthread_create(&threadID[i], &thread_attr, thread_print_func, &i);
         if(0 != ErrCode)
         {
             printf("create a thread fail.\n");
             return 0;
         }
+        pthread_mutex_lock(&mutex);
+        thread_exit_flag[i] = 0;
+        pthread_mutex_unlock(&mutex);
         printf("create %d a thread success.\n", i);
     }
 
+#if 0
     for(int j = 0; j < 5; j++)
     {
         ErrCode = pthread_join(threadID[j], NULL);
@@ -52,7 +84,23 @@ int main()
         }
         printf("thread %d exit successfully.\n", j);
     }
+#endif
+
+    while(1)
+    {
+        if((1 == thread_exit_flag[0])
+            &&(1 == thread_exit_flag[1])
+            &&(1 == thread_exit_flag[2])
+            &&(1 == thread_exit_flag[3])
+            &&(1 == thread_exit_flag[4])
+        )
+        {
+            printf("all thread exit success.\n");
+            break;
+        }
+    }
 
     pthread_mutex_destroy(&mutex);
+    pthread_attr_destroy(&thread_attr);
     return 0;
 }
